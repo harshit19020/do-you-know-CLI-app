3f0581fb38b514a5a385983a1231b4677d0de9cb
Samkit Jain <87150585+samkitjain1802@users.noreply.github.com>
2021-07-20 08:14:47 +0000

    slight modification in output

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ var readlineSync = require("readline-sync");
 var score = 0;
 
 var questions = [{
-  q: "Where do I live? ",
+  q: "Where do samkit live? ",
   a: "Jaipur"
 }, {
-  q: "My favorite superhero would be? ",
-  a: "Iron Man"
-},
-{
-  q: "Where do I work? ",
-  a: "At home"
+  q: "which college samkit is presently studying ? ",
+  a: "jecrc foundation"
+}, {
+  q: "his birthday in dd/mm/yyyy ",
+  a: "18/02/2001"
+}, {
+  q: "his favourite sport ",
+  a: "cricket"
+}, {
+  q: "What's his current age? ",
+  a: "20"
 }];
 
 function welcome() {
  var userName = readlineSync.question("What's your name? ");
 
   console.log("Welcome "+ userName + " to DO YOU KNOW Samkit?");
+  console.log("lets play it \n");
 }
 
 function play(question, answer) {
@@ -29,6 +35,7 @@ function play(question, answer) {
     
   } else {
     console.log("wrong!");
+    console.log("the correct answer is " + answer);
    
   }
 
@@ -46,7 +53,7 @@ function game() {
 function showScores() {
   console.log("YAY! You SCORED: ", score);
 
-  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+  // console.log("Check out the high scores, if you should be there ping me and I'll update it");
 }
 
 
